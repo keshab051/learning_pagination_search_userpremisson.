dac30e9b6329a1a6ce2adf2661bd12bf36a75f10
KeshabNeupane <keshabneupane061@gmail.com>
2025-01-25 22:52:42 +0545

    pagination of page completed

--- newapp/views.py
+++ newapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Movies
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def movie_list(request):
    movie_objects = Movies.objects.all()
+   paginator = Paginator(movie_objects,4)
+   page = request.GET.get('page') 
+   movie_objects = paginator.get_page(page)
    return render(request,'newapp/movies_list.html',{'movies_objects':movie_objects})
    # return HttpResponse("hello")
